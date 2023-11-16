332a7cee7eab8c22e4c4ef056bb4d7731dfd681d
Pat173 <juan.ortiz1734@gmail.com>
2023-11-16 13:46:16 +0100

    tick rate & layers
    tick rate and layers updated

--- Netball/Netball/Assets/Scripts/PlayerNetwork.cs
+++ Netball/Netball/Assets/Scripts/PlayerNetwork.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Multiplayer.Netball.ClientAuthority;
 using Unity.Netcode;
 using Unity.Netcode.Transports.UTP;
 using UnityEngine;
